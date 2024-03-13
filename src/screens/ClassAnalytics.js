4160ff5550deb8a0962a143c6716a66f87b474eb
Legus-Yeung <legus.yeung@hotmail.com>
2024-03-13 13:45:45 -0400

    minor changes

--- src/screens/ClassAnalytics.js
+++ src/screens/ClassAnalytics.js
@@ -354,7 +354,7 @@ export default function ClassAnalytics({ route }) {
             </View>
           ))}
         </ScrollView>
-        <View style={{justifyContent: 'flex-end', paddingHorizontal: 20, paddingBottom: 5}}>
+        <ScrollView style={{justifyContent: 'flex-end', paddingHorizontal: 20, paddingBottom: 5}}>
             <Text></Text>
             <Pressable
               style={style.genericButton}
@@ -362,7 +362,7 @@ export default function ClassAnalytics({ route }) {
             >
               <Text style={style.genericButtonText}>Close</Text>
             </Pressable>
-          </View>
+          </ScrollView>
       </Modal>
     );
   };
